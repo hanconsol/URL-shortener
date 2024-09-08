c906b43ecf8d13636a866fb2980fb6219665f256
hanconsol <hasfaraj49@gmail.com>
2024-09-07 22:26:50 -0500

    feat: add url validator

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const Url = require('./models/url.model.js');
+const dns = require('dns');
+const { error } = require('console');
 const app = express();
 
 // Basic Configuration
@@ -15,33 +17,50 @@ app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.use(express.json());
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
 // add post
 app.post('/api/shorturl', async (req, res) => {
- try{
-    const url = await Url.create(req.body);
-    res.json(url);
- }
- catch(error) {
-     res.json({error: "Content not posted"})
- }
+  try {
+
+    const original_url = req.body.url.toString()
+    const hostname = original_url.replace("https://www.", "");
+
+    dns.lookup(hostname, (err, address, family) => {
+      console.log(original_url, hostname);
+      if (err) {
+        console.error(err)
+        res.json({ error: "invalid url" })
+      } else {
+        const url = Url.create(req.body);
+
+        res.json({ original_url: original_url });
+        console.log("ip_address = ", address);
+
+      }
+
+    });
+   
+  }
+  catch (error) {
+    res.json({ error: "Content not posted" })
+  }
 })
 
-  mongoose.connect(process.env.MONGO_URI) 
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Database is connected!");
-  app.listen(port, function() {
-     console.log(`Listening on port ${port}`)
+    app.listen(port, function () {
+      console.log(`Listening on port ${port}`)
     });
   })
   .catch((err) => console.log("Error: Database not connected"));
